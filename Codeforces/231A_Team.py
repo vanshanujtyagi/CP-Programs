47fba4e72f1b2aedeb45393b76773fe5381dab23
Vansh Tyagi <vanshanujtyagi@gmail.com>
2026-07-28 11:52:57 +0530

    Create 231A_Team.py
    One day three best friends Petya, Vasya and Tonya decided to form a team and take part in programming contests. Participants are usually offered several problems during programming contests. Long before the start the friends decided that they will implement a problem if at least two of them are sure about the solution. Otherwise, the friends won't write the problem's solution.
    
    This contest offers n problems to the participants. For each problem we know, which friend is sure about the solution. Help the friends find the number of problems for which they will write a solution.
    
    Input
    The first input line contains a single integer n (1 ≤ n ≤ 1000) — the number of problems in the contest. Then n lines contain three integers each, each integer is either 0 or 1. If the first number in the line equals 1, then Petya is sure about the problem's solution, otherwise he isn't sure. The second number shows Vasya's view on the solution, the third number shows Tonya's view. The numbers on the lines are separated by spaces.
    
    Output
    Print a single integer — the number of problems the friends will implement on the contest.

---
+++ Codeforces/231A_Team.py
@@ -0,0 +1,7 @@
+n=int(input()) #asks for number of problems
+count=0
+for i in range(n): #n lines
+    a,b,c=map(int,input().split()) #if Petya, Vasya, Tonya are sure or unsure
+    if a+b+c>=2:
+        count=count+1 #counts the problem if above cond. holds true
+print(count)
