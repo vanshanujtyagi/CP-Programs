19af076f9ed8eed6a13366910c908dd078c8208b
Vansh Tyagi <vanshanujtyagi@gmail.com>
2026-07-29 12:15:27 +0530

    281A
    Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
    
    Note, that during capitalization all the letters except the first one remains unchanged.
    
    Input
    A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. The length of the word will not exceed 103.
    
    Output
    Output the given word after capitalization.

---
+++ Codeforces/Word Capitalisation.py
@@ -0,0 +1,2 @@
+line=input() #asks for a string. String by default in python is indexed like lists.
+print(line[0].upper()+line[1:]) #line[0].upper() capitalizes the first letter of the string. +line[1:] add the string except the 0 indexed first letter.
