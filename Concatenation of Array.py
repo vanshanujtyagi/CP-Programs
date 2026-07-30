ca52e645571adc1ea2fcf0b0ad6d5cac8af9160d
Vansh Tyagi <vanshanujtyagi@gmail.com>
2026-07-30 15:07:15 +0530

    1929
    Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
    
    Specifically, ans is the concatenation of two nums arrays.
    
    Return the array ans.

---
+++ Concatenation of Array.py
@@ -0,0 +1,4 @@
+class Solution: #leetcode default
+    def getConcatenation(self, nums: List[int]) -> List[int]: #leetcode default for nums=list(map(int,input().split()))
+        ans=nums+nums #concatenates or just add the list/array two times into one.
+        return ans #prints the concatenated list/array
