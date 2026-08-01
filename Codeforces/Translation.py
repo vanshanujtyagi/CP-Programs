533bd790c2bbb5f0260c906af9ecb05208d68983
Vansh Tyagi <vanshanujtyagi@gmail.com>
2026-08-01 17:10:06 +0530

    41A
    The translation from the Berland language into the Birland language is not an easy task. Those languages are very similar: a Berlandish word differs from a Birlandish word with the same meaning a little: it is spelled (and pronounced) reversely. For example, a Berlandish word code corresponds to a Birlandish word edoc. However, making a mistake during the "translation" is easy. Vasya translated the word s from Berlandish into Birlandish as t. Help him: find out if he translated the word correctly.
    
    Input
    The first line contains word s, the second line contains word t. The words consist of lowercase Latin letters. The input data do not contain unnecessary spaces. The words are not empty and their lengths do not exceed 100 symbols.
    
    Output
    If the word t is a word s, written reversely, print YES, otherwise print NO.

---
+++ Codeforces/Translation.py
@@ -0,0 +1,6 @@
+s=input() #the actual word ie word in berlandish
+t=input() #the reversed word ie word in birlandish 
+if s[::-1]==t: #if reverse of s is equals to t
+    print("YES") #print yes
+else: #otherwise, if its not equal
+    print("NO") #print no
