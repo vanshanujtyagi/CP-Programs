283c15d9c801d40ac7e8d290a102e7e7f664bc48
Vansh Tyagi <vanshanujtyagi@gmail.com>
2026-08-12 09:57:38 +0530

    144A
    A Ministry for Defense sent a general to inspect the Super Secret Military Squad under the command of the Colonel SuperDuper. Having learned the news, the colonel ordered to all n squad soldiers to line up on the parade ground.
    
    By the military charter the soldiers should stand in the order of non-increasing of their height. But as there's virtually no time to do that, the soldiers lined up in the arbitrary order. However, the general is rather short-sighted and he thinks that the soldiers lined up correctly if the first soldier in the line has the maximum height and the last soldier has the minimum height. Please note that the way other solders are positioned does not matter, including the case when there are several soldiers whose height is maximum or minimum. Only the heights of the first and the last soldier are important.
    
    For example, the general considers the sequence of heights (4, 3, 4, 2, 1, 1) correct and the sequence (4, 3, 1, 2, 2) wrong.
    
    Within one second the colonel can swap any two neighboring soldiers. Help him count the minimum time needed to form a line-up which the general will consider correct.
    
    Input
    The first input line contains the only integer n (2 ≤ n ≤ 100) which represents the number of soldiers in the line. The second line contains integers a1, a2, ..., an (1 ≤ ai ≤ 100) the values of the soldiers' heights in the order of soldiers' heights' increasing in the order from the beginning of the line to its end. The numbers are space-separated. Numbers a1, a2, ..., an are not necessarily different.
    
    Output
    Print the only integer — the minimum number of seconds the colonel will need to form a line-up the general will like.

--- Codeforces/Arrival of General.py
+++ Codeforces/Arrival of General.py
@@ -1,10 +1,10 @@
-n = int(input())
-s = list(map(int, input().split()))
+n = int(input()) #take the input of number of soldiers
+s = list(map(int, input().split()))  #make list of heights of soldiers
 
-maxindex = s.index(max(s))
-minindex = n - 1 - s[::-1].index(min(s))
+maxindex = s.index(max(s)) #give the index of the maximum height
+minindex = n - 1 - s[::-1].index(min(s)) #give the index of the minimum height, reversed the string so that we can find the index of last value in case of repeated occurence of same value.
 
-if maxindex > minindex:
-    print(maxindex + (n - 1 - minindex) - 1)
-else:
+if maxindex > minindex: #if maxindex is greater than minindex, both will swap each other at some time.
+    print(maxindex + (n - 1 - minindex) - 1) #thats way -1 to compensate.
+else: 
     print(maxindex + (n - 1 - minindex))
