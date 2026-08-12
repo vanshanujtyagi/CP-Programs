71cfe95928b285a886607d9f38b0d3c4038317fb
Vansh Tyagi <vanshanujtyagi@gmail.com>
2026-08-12 10:05:57 +0530

    155A
    Vasya adores sport programming. He can't write programs but he loves to watch the contests' progress. Vasya even has a favorite coder and Vasya pays special attention to him.
    
    One day Vasya decided to collect the results of all contests where his favorite coder participated and track the progress of his coolness. For each contest where this coder participated, he wrote out a single non-negative number — the number of points his favorite coder earned in the contest. Vasya wrote out the points for the contest in the order, in which the contests run (naturally, no two contests ran simultaneously).
    
    Vasya considers a coder's performance in a contest amazing in two situations: he can break either his best or his worst performance record. First, it is amazing if during the contest the coder earns strictly more points that he earned on each past contest. Second, it is amazing if during the contest the coder earns strictly less points that he earned on each past contest. A coder's first contest isn't considered amazing. Now he wants to count the number of amazing performances the coder had throughout his whole history of participating in contests. But the list of earned points turned out long and Vasya can't code... That's why he asks you to help him.
    
    Input
    The first line contains the single integer n (1 ≤ n ≤ 1000) — the number of contests where the coder participated.
    
    The next line contains n space-separated non-negative integer numbers — they are the points which the coder has earned. The points are given in the chronological order. All points do not exceed 10000.
    
    Output
    Print the single number — the number of amazing performances the coder has had during his whole history of participating in the contests.

--- Codeforces/I_love_%username%.py
+++ Codeforces/I_love_%username%.py
@@ -1,13 +1,13 @@
 n=int(input())
 scores=list(map(int,input().split()))
-maximum=scores[0]
-minimum=scores[0]
-count=0
-for i in range(1,len(scores)):
-    if scores[i]>maximum:
-        count+=1
-        maximum=scores[i]
-    elif scores[i]<minimum:
-        count+=1
-        minimum=scores[i]
-print(count)
+maximum=scores[0] #initial maximum score is the first one.
+minimum=scores[0] #initial minimum score is the first one.
+count=0 #initial count is 0.
+for i in range(1,len(scores)): #loop for all indexes of list scores, starting from 1.
+    if scores[i]>maximum: #if value at index i, starting from 1, is greater than the maximum, initially the first value.
+        count+=1 #increase the count.
+        maximum=scores[i] #new maximum is the value at the i index.
+    elif scores[i]<minimum: #if value at index i, starting from 1, is smaller than the minimum, initially the first value. 
+        count+=1 #increase count.
+        minimum=scores[i] #the new minimum is the value at the i index.
+print(count) #print count.
