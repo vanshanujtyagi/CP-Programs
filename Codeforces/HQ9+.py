d0b8c20099266f29a32270c8fd43d9031a2bb1ed
Vansh Tyagi <vanshanujtyagi@gmail.com>
2026-08-12 10:00:50 +0530

    133A
    HQ9+ is a joke programming language which has only four one-character instructions:
    
    "H" prints "Hello, World!",
    "Q" prints the source code of the program itself,
    "9" prints the lyrics of "99 Bottles of Beer" song,
    "+" increments the value stored in the internal accumulator.
    Instructions "H" and "Q" are case-sensitive and must be uppercase. The characters of the program which are not instructions are ignored.
    
    You are given a program written in HQ9+. You have to figure out whether executing this program will produce any output.
    
    Input
    The input will consist of a single line p which will give a program in HQ9+. String p will contain between 1 and 100 characters, inclusive. ASCII-code of each character of p will be between 33 (exclamation mark) and 126 (tilde), inclusive.
    
    Output
    Output "YES", if executing the program will produce any output, and "NO" otherwise.

--- Codeforces/HQ9+.py
+++ Codeforces/HQ9+.py
@@ -1,5 +1,7 @@
-p = input()
-if 'H' in p or 'Q' in p or '9' in p:
-    print('YES')
-else:
-    print('NO')
+p = input() #take the input
+if 'H' in p or 'Q' in p or '9' in p: #if p contains H,Q,9,
+    print('YES') #then print YES
+else: #if not, 
+    print('NO') #print NO
+
+#'+' gives no output, so it is not considered
